46702d462d08d19a8f3e14d4e9e394d0343e54c7
riskgame15 <tttungcr@gmail.com>
2025-03-13 22:23:41 +0700

    jwt-ajax

---
+++ jwt-ajax/login/login.js
@@ -0,0 +1,26 @@
+function login() {
+    event.preventDefault();
+//    lay du lieu
+    let name = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    let user={
+        "username": name,
+        "password": password,
+    }
+    $.ajax({
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+        url: "http://localhost:8080/api/login",
+        type: "POST",
+        data: JSON.stringify(user),
+        success: function (result) {
+            // console.log(result);
+            localStorage.setItem("token", result.token);
+            localStorage.setItem("name",result.name);
+            window.location.href = "../product/index.html"
+        }
+    })
+
+}
